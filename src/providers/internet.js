becec7a221b4666c08189224cd6867fca23d10ef
ProjectSoulbyTmb <dreambot333@gmail.com>
2026-08-21 19:43:29 -0500

    Strip UTF-8 BOMs repo-wide and restore v1.0.0 structured guards lost in concurrent sync

--- src/providers/internet.js
+++ src/providers/internet.js
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
+// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
 // SPDX-License-Identifier: LicenseRef-Eidovara-Source-Available-1.0
 import { isExplicitInternetRequest } from '../core/workspace.js';
 import { officialSearchHandoffs } from '../core/entertainment.js';
