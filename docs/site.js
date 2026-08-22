b6fc20959cc14ee708516c19d21bf13e18428b5d
Eidovara Project <eidovara@project-soul.com>
2026-08-21 20:52:56 -0500

    Fix encoding corruption in all HTML/CSS/JS files (em dashes, bullets, copyright symbols, emoji)

--- docs/site.js
+++ docs/site.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
+﻿// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
 // SPDX-License-Identifier: LicenseRef-Eidovara-Source-Available-1.0
 /**
  * EIDOVARA WEBSITE ENTRY POINT
@@ -580,4 +580,5 @@ try {
     }
   };
 })();
+
 
