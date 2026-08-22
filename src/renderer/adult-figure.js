40ad7acbb2afc53c5d1547d5f47a0f15be6c7757
ProjectSoulbyTmb <dreambot333@gmail.com>
2026-08-21 19:43:34 -0500

    Strip UTF-8 BOMs repo-wide and restore v1.0.0 structured guards lost in concurrent sync

--- src/renderer/adult-figure.js
+++ src/renderer/adult-figure.js
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
+// SPDX-FileCopyrightText: 2026 Soul Consciousness Studios
 // SPDX-License-Identifier: LicenseRef-Eidovara-Source-Available-1.0
 import { buildAdultMesh, hexRgb, FIGURE_BACKEND, FIGURE_QUALITY } from '../core/adult-mesh.js';
 import { avatarLayout } from '../core/adult-soul.js';
